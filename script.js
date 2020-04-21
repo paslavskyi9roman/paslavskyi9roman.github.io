2dbdcd7f7443e4129c6023d732a9ac57cd74ca83
paslavskyi9roman <paslavsky.roman@gmail.com>
2020-04-21 16:26:37 +0300

    🚗

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const toggle = document.getElementById('toggle');
+const open = document.getElementById('open');
 const expandCards = document.getElementById('expand-cards');
 const close = document.getElementById('close');
 
 // Expand cards-section
-toggle.addEventListener('click', () => {
+open.addEventListener('click', () => {
   expandCards.classList.add('show-cards');
-  toggle.classList.add('hide');
+  open.classList.add('hide');
+  close.classList.remove('hide');
 });
 
 // Hide cards-section
 close.addEventListener('click', () => {
   expandCards.classList.remove('show-cards');
-  toggle.classList.remove('hide');
+  close.classList.add('hide');
+  open.classList.remove('hide');
 });
