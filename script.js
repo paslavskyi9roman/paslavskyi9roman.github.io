fd69f1485fa9201264167d15a98ef9591d75a584
paslavskyi9roman <paslavsky.roman@gmail.com>
2020-04-21 15:33:22 +0300

    - Music player added, - Expand button added, - Bug with <p> added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const toggle = document.getElementById('open-button');
-const projects = document.getElementsByClassName('projects');
+const toggle = document.getElementById('toggle');
+const expandCards = document.getElementById('expand-cards');
+const close = document.getElementById('close');
 
+// Expand cards-section
 toggle.addEventListener('click', () => {
-  projects.classList.add('expand');
+  expandCards.classList.add('show-cards');
+  toggle.classList.add('hide');
+});
+
+// Hide cards-section
+close.addEventListener('click', () => {
+  expandCards.classList.remove('show-cards');
+  toggle.classList.remove('hide');
 });
